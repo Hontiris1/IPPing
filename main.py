57cc05529fcd9b79a085ffbdf01d4c283c79ab8a
Hontiris1 <56845190+Hontiris1@users.noreply.github.com>
2019-11-26 00:10:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,86 +1,41 @@
-import os
+from ipfunc import *
+import ipfunc as ipfunc
 import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
 
-def close_window (root):
-    root.destroy()
 
-#This will add a new IP to the IPlist
-def newip():
-    data = e1.get()
+class GUIframework(ipfunc.GUIframework):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
 
-    if data == "":
-        print("Please enter an IP address")
-    else:
-        ipopen = open("IPlist.txt")
-        with open('IPlist.txt', mode='a') as add:
-            add.write("\n{}".format(data))
-            print("Ip address {} has been added!".format(data))
-            ipopen.close()
-            label.grid_forget()
+        self.test = tk.Label(self, text="___________").grid(row=1)
+        self.iptxt = tk.Label(self, text="Enter IP Address").grid(row=2)
 
-#This will read each line on the IPlist.txt file
-def contents():
-    ipopen = open("IPlist.txt")
+        tk.Button(self, text='Add New IP', command=self.newip).grid(row=0, column=0, pady=2),
+        tk.Button(self, text='List of IPs', command=self.contents).grid(row=0, column=1, sticky=tk.SE,pady=2),
+        tk.Button(self, text='Scan all IPs', command=self.scanlist).grid(row=0, column=2, sticky=tk.SE,pady=2),
+        tk.Button(self, text='Quit', command=self.quit).grid(row=0, column=3, pady=2)
 
-    #Opens the IPlist.txt file and strips each of the lines so that we can read individually.
-    with open("IPlist.txt", "r+") as ips_file:
-        ips = [ip.strip() for ip in ips_file.readlines()]
-
-    with open("IPlist.txt", mode='r') as read:
-        for ip in ips:
-            print(ip)
-            #print(ip)
-            #ipopen.seek(0)
-            ipopen.close()
-    return
-
-def scanlist():
-    import time
-    progress = Progressbar(master, orient=HORIZONTAL, length=50, mode='indeterminate')
-    progress.grid()
-    master.update_idletasks()
-    ipopen = open("IPlist.txt")
-
-    #Opens the IPlist.txt file and strips each of the lines so that we can read individually.
-    with open("IPlist.txt", "r+") as ips_file:
-        ips = [ip.strip() for ip in ips_file.readlines()]
-        progress['value'] = 0
-        master.update_idletasks()
-        time.sleep(0.5)
-
-    #Read each line from the IPlist.txt file
-    with open("IPlist.txt", "r") as available_ips_file:
-        for ip in ips:  #Pings each line from the IPlist.txt file
-            response = os.system('ping -a -n 1 {}'.format(ip))
-            progress['value'] += 50
-            master.update_idletasks()
-            time.sleep(0.5)
-
-            if response == 0:   #Up
-                print("- Ip Address:", ip, 'is up!')
-            elif response == 512:   #Down
-                print("- IP Address:", ip, 'is down!')
-            else:   #other error
-                print("- Bad parameters or other error!")
-
-    return progress.grid_forget()
-
-master = tk.Tk()
-master.title("IPPing")
-master.geometry("350x100+900+300")
-
-Label(master, text="___________").grid(row=1)
-Label(master, text="Enter IP Address").grid(row=2)
-
-e1 = tk.Entry(master)
-e1.grid(row=2, column=1)
-
-tk.Button(master,text='Add New IP', command=newip).grid(row=0,column=0,pady=2)
-tk.Button(master,text='List of IPs', command=contents).grid(row=0,column=1,sticky=tk.SE,pady=2)
-tk.Button(master,text='Ping all servers', command=scanlist).grid(row=0,column=2,sticky=tk.SE,pady=2)
-tk.Button(master,text='Quit',command=master.quit).grid(row=0,column=3,pady=2)
+        self.e1 = tk.Entry(self)
+        self.e1.grid(row=2, column=1)
 
 
-tk.mainloop()
+class Contents(ipfunc.GUIframework):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+
+
+class App(tk.Tk):
+     def __init__(self):
+         super().__init__()
+
+         self.title("IPPing")
+         self.geometry("340x130+900+300")
+
+         self.ip_function = GUIframework(self)
+         self.ip_function.grid()
+
+if __name__ == '__main__':
+    App().mainloop()
